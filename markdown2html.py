5b93f99f5f614aab0427dc907758a83d5fffede2
Jose A. Vallejo <josealfredovallejo25@gmail.com>
2021-05-02 15:54:35 -0500

    task 0 done

--- markdown2html.py
+++ markdown2html.py
@@ -1,27 +1,32 @@
 import sys
 
 
-def olHandle(data):
+def ol_parse(data):
     return f'<ol></ol>'
 
 
-def ulHandle(data):
+def ul_parse(data):
     return f'<ul></ul>'
 
 
-def headingsHandle(data):
+def heading_parse(index, lines_read_list):
     """heading tags"""
-    return f'<h?></h?>'
-    # data = data.lstrip('#').strip()
-    # if 1 <= count <= 6:
-    #     write.append(f'<h{count}>{data}</h{count}>')
-    # return write
+    # data = data.strip()
+    # min_level = 1
+    # max_level = 6
+    # heading_level = len(data) - len(data.lstrip('#'))
+
+    # if min_level <= heading_level <= max_level:
+    #     string_to_parsing = data.lstrip("#").strip()
+    #     return f'<h{heading_level}>{string_to_parsing}</h{heading_level}>\n'
+
+    return None
 
 
-funtion_convert = {
-    '#': headingsHandle,
-    '-': ulHandle,
-    '*': olHandle,
+funtion_parsing = {
+    '#': heading_parse,
+    '-': ul_parse,
+    '*': ol_parse,
 }
 
 if __name__ == '__main__':
@@ -29,24 +34,24 @@ if __name__ == '__main__':
         sys.stderr.write('Usage: ./markdown2html.py README.md README.html\n')
         sys.exit(1)
     try:
+        htmlTagList = []
         with open(sys.argv[1], 'r') as markdownFile:
-            htmlTagList = []
-            linesRead = markdownFile.readlines()
-            for line in linesRead:
-                line = line.strip()
-                firstChar = line[0]
-                if firstChar in funtion_convert.keys():
-                    htmlTag = funtion_convert[firstChar](line)
+            lines_read_list = markdownFile.readlines()
+            for index in range(len(linesRead)):
+                line = lines_read_list[index].strip()
+                first_char = line[0]
+                if first_char in funtion_parsing.keys():
+                    htmlTag = funtion_parsing[first_char](index, lines_read_list)
                 else:
                     htmlTag = 'parrafo'
                 htmlTagList.append(htmlTag)
 
             print(htmlTagList)
-        # Write into an html file
+
         # with open(sys.argv[2], 'w', encoding="utf-8") as html:
-        #     content = '\n'.join([str(char)
-        #                          for elem in write for char in elem])
-        #     html.write(content + '\n')
+        #     for htmlLine in htmlTagList:
+        #         if htmlLine:
+        #             html.write(htmlLine)
         # sys.exit(0)
     except FileNotFoundError:
         sys.stderr.write('Missing {}\n'.format(sys.argv[1]))
